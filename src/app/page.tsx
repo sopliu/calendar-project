09cce3e50a99e72af068952f09e7215b5f91688b
sopliu <sophie.liu5803@gmail.com>
2024-12-31 21:48:01 +1100

    Redirect users on root route to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./page.module.css";
 import { useEffect } from "react";
-import { redirect, usePathname } from "next/navigation";
-import { Button } from "@mui/material";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const pathname = usePathname();
-  // useEffect(() => {
-  //   if (!user && !["/login", "/signup"].includes(pathname)) {
-  //     // go to login page if not in login or sign up and is not signed in
-  //     redirect("/login");
-  //   }
-  //   console.log(pathname);
-  // }, [pathname, user]);
-
-  return <Button onClick={() => redirect("/login")}>Go to Login</Button>;
+  useEffect(() => {
+    redirect("/login");
+  }, []);
 }
