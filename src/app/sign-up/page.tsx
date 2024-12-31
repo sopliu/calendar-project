b6f54b6f6b1b80664da8ee5bf85f8cac88b0d87e
sopliu <sophie.liu5803@gmail.com>
2024-12-31 19:17:01 +1100

    Created sign up page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "./sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
